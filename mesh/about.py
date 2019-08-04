fd34e373bd78c528e2eba80fb24d8be88eab7d62
Jacques du Toit <jacques@spatialedge.co.za>
2019-08-04 18:50:53 +0000

    added packaging

---
+++ mesh/about.py
@@ -0,0 +1,21 @@
+import mesh.__init__
+
+
+name = "mesh"
+description = (
+    "Facades and common functions necessary for data science and data engineering workflows"
+)
+version = mesh.__init__.__version__
+author = "Jacques du Toit"
+email = ""
+uri = "https://github.com/jackdotwa/analytics-mesh"
+keywords = 'analytics, utilities'
+classifiers = [
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Framework :: Jupyter, Python3',
+    'License :: Apache2',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Utilities'
+]
