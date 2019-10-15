137d4cfaa43b67936ce1f98038b8dca78d9185da
jackdotwa <jacques@spatialedge.co.za>
2019-10-15 14:09:50 +0200

    renamed function to be more descriptive and added test cases

--- mesh/dates/wrangler.py
+++ mesh/dates/wrangler.py
@@ -4,23 +4,26 @@ from datetime import datetime
 log = logging.getLogger(__name__)
 
 
-def date_formatter(string_date, day):
+def yyyymmdd_to_datetime(string_date: str, day: int = None) -> datetime:
     """
     The day modifier is optional, if a complete string date is provided then simply return
     the object to that date, and ignore the day_mod
     :param string_date: a string date of YYYYMMDD or YYYYMM. The former overrides the day parameter (causes
     it to be ignored), but YYYYMM appends day
+    :param day: optional day integer specifying the day to add to the date
     :return:
     """
-    format = '%Y%m%d'
+    string_format = '%Y%m%d'
     if len(string_date) == 8:
         log.warning('a specific date has been specified')
-        return datetime.strptime(string_date, format)
+        return datetime.strptime(string_date, string_format)
     else:
         log.debug('modifying/augmenting the date with the correct run day')
         if len(string_date) != 6:
             raise ValueError('expected YYYYMM format as day modifier is to be applied')
+        elif day is None:
+            raise ValueError('if string date is YYYYMM, then day is expected')
         else:
             if isinstance(day, int):
                 day = '{:02d}'.format(day)
-            return datetime.strptime('{yyyymm}{dd}'.format(yyyymm=string_date, dd=day), format)
+            return datetime.strptime('{yyyymm}{dd}'.format(yyyymm=string_date, dd=day), string_format)
--- tests/test_dates_wrangler.py
+++ tests/test_dates_wrangler.py
@@ -6,7 +6,15 @@ from datetime import datetime
 class TestDateFormatter(unittest.TestCase):
 
     def testYYYYMM(self):
-        self.assertEqual(datetime(2020, 1, 1), mesh.dates.wrangler.date_formatter('202001', 1))
+        self.assertEqual(datetime(2020, 1, 1), mesh.dates.wrangler.yyyymmdd_to_datetime('202001', 1))
 
     def testYYYYMMDD(self):
-        self.assertEqual(datetime(2020, 1, 20), mesh.dates.wrangler.date_formatter('20200120', 1))
+        self.assertEqual(datetime(2020, 1, 20), mesh.dates.wrangler.yyyymmdd_to_datetime('20200120', 1))
+        self.assertEqual(datetime(2020, 1, 20), mesh.dates.wrangler.yyyymmdd_to_datetime('20200120'))
+
+    def testNegative(self):
+        with self.assertRaises(ValueError):
+            mesh.dates.wrangler.yyyymmdd_to_datetime('2010')
+        with self.assertRaises(ValueError):
+            self.assertEqual(datetime(2020, 1, 1), mesh.dates.wrangler.yyyymmdd_to_datetime('202001'))
+
